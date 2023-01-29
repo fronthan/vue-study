ef34d9fc3040760f7104e9c0ebad4152fcdae619
fronthan <cynthia0330@kakao.com>
2023-01-29 20:23:52 +0900

    연습

---
+++ vue-lotto/src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>가장 많이 나온 숫자 top 7</h1>
+    <Numbers />
+  </div>
+</template>
+<script>
+import Numbers from '../components/Numbers.vue'
+export default {
+  components: {
+    Numbers
+  }
+}
+</script>
+<style>
+  
+</style>
